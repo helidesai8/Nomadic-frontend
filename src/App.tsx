6e364f257df39e842ad7576cd82c78972c0d28fa
Heli Desai <hl560931@dal.ca>
2024-12-02 00:35:47 -0400

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -40,31 +40,31 @@ function App() {
                     <ToastContainer />
                     <Routes>
                         <Route element={<Protected />}>
-                            <Route path="/manage" element={role==="ADMIN"?<Manage_Listing />:<HomePage/>} />
-                            <Route path="/manage/add-tour" element={role==="ADMIN"?<AddTourPage />:<HomePage/>} />
-                            <Route path="/manage/plan-details/:id" element={role==="ADMIN"?<PlanDetails />:<HomePage/>} />
-                            <Route path="/manage/create-package" element={role==="ADMIN"?<CreateTourPackage />:<HomePage/>} />
-                            <Route path="/manage/blog" element={role==="ADMIN"?<ManageBlogList />:<HomePage/>} />
-                            <Route path="/manage/blog/add" element={role==="ADMIN"?<AddBlogForm />:<HomePage/>} />
-                            <Route path="/manage/blog/:blogId/update" element={role==="ADMIN"?<UpdateBlogForm />:<HomePage/>} />
-                            <Route path="/wishlist" element={<Wishlist />} />
-                            <Route path="/history/:id" element={<HistoryPage />} />
-                            <Route path="/reviews" element={<ReviewForm />} />
-                            <Route path="/analytics" element={role==="ADMIN"?<AnalyticsPage />:<HomePage/>}/>
+                            <Route path="manage" element={role==="ADMIN"?<Manage_Listing />:<HomePage/>} />
+                            <Route path="manage/add-tour" element={role==="ADMIN"?<AddTourPage />:<HomePage/>} />
+                            <Route path="manage/plan-details/:id" element={role==="ADMIN"?<PlanDetails />:<HomePage/>} />
+                            <Route path="manage/create-package" element={role==="ADMIN"?<CreateTourPackage />:<HomePage/>} />
+                            <Route path="manage/blog" element={role==="ADMIN"?<ManageBlogList />:<HomePage/>} />
+                            <Route path="manage/blog/add" element={role==="ADMIN"?<AddBlogForm />:<HomePage/>} />
+                            <Route path="manage/blog/:blogId/update" element={role==="ADMIN"?<UpdateBlogForm />:<HomePage/>} />
+                            <Route path="wishlist" element={<Wishlist />} />
+                            <Route path="history/:id" element={<HistoryPage />} />
+                            <Route path="reviews" element={<ReviewForm />} />
+                            <Route path="analytics" element={role==="ADMIN"?<AnalyticsPage />:<HomePage/>}/>
                         </Route>
-                        <Route path="/contactus" element={<ContactUsPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/signup" element={<SignUpPage />} />
-                        <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-                        <Route path="/reset-password/:token" element={<ResetPassword />} />
-                        <Route path="/blogs" element={<Blog />} />
-                        <Route path="/blogs/:id" element={<BlogDetailsPage />} />
-                        <Route path="/tours/:id" element={<TourDetail />} />
-                        <Route path="/search" element={<SearchPage />} />
-                        <Route path="/faq" element={<FAQPage />} />
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/signup" element={<SignUpPage />} />
+                        <Route path="contactus" element={<ContactUsPage />} />
+                        <Route path="login" element={<LoginPage />} />
+                        <Route path="signup" element={<SignUpPage />} />
+                        <Route path="forgotpassword" element={<ForgotPasswordPage />} />
+                        <Route path="reset-password/:token" element={<ResetPassword />} />
+                        <Route path="blogs" element={<Blog />} />
+                        <Route path="blogs/:id" element={<BlogDetailsPage />} />
+                        <Route path="tours/:id" element={<TourDetail />} />
+                        <Route path="search" element={<SearchPage />} />
+                        <Route path="faq" element={<FAQPage />} />
+                        <Route path="" element={<HomePage />} />
+                        <Route path="login" element={<LoginPage />} />
+                        <Route path="signup" element={<SignUpPage />} />
                     </Routes>
                     <ScrollToTop />
                 </Router>
